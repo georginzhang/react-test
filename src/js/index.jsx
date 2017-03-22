9afea5ee67484b400a18a003b3b3b2a61d5a3b27
zhangwei <zhangwei@wei.com>
2017-03-22 18:29:07 +0800

    add

---
+++ src/js/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(
+    <h1>123456777777</h1>,
+    document.getElementById('box')
+)
---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    entry: './src/js/index.jsx',
+    output: {
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [{
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            query: {
+                presets: ['react', 'es2015']
+            }
+        }]
+    }
+}
